24e787bb4da6d54c810e9f107531e70c42066ab8
saurabh jais <128085664+saurabhjais12@users.noreply.github.com>
2025-04-20 13:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	rreturn new TextEncoder().encode(str).length;
 	
 };
 
 // Do not change the code below
-// const str = prompt("Enter some string.");
+const str = prompt("Enter some string.");
 alert(byteSize(str));
