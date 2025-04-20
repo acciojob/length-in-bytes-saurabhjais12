917afa8fd92e5cccddc637e1e5814616b2ee5674
saurabh jais <128085664+saurabhjais12@users.noreply.github.com>
2025-04-20 13:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const byteSize = (str) => {
-  // write your code here
-	rreturn new TextEncoder().encode(str).length;
-	
+  // Correct the typo and return the byte size of the string
+  return new TextEncoder().encode(str).length;
 };
 
 // Do not change the code below
